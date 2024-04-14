e69b10a3b2d64eab7696fbe652a04c853da0b138
twisterjafla <122408604+twisterjafla@users.noreply.github.com>
2024-04-14 16:37:18 -0600

    added some stuff

--- src/main/java/frc/robot/SwerveConstants.java
+++ src/main/java/frc/robot/SwerveConstants.java
@@ -1,28 +1,53 @@
 package frc.robot;
 
+import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
+
 public class SwerveConstants {
     public static class frontRight{
-        double Xoffset;
-        double Yoffset;
-        double driveID;
-        double turnID;
+        public static final double Xoffset=0;
+        public static final double Yoffset=0;
+        public static final int driveID=0;
+        public static final int turnID=0;
+        public static final boolean driveInverted=false;
+        public static final boolean steerInverted=false;
+        public static final SwerveModuleConstants frontRightConstants = new SwerveModuleConstants().withLocationX(Xoffset).withLocationY(Yoffset)
+        .withDriveMotorId(driveID).withSteerMotorId(turnID).withDriveMotorInverted(driveInverted).withSteerMotorInverted(steerInverted);
 
     }
     public static class frontLeft{
-        double Xoffset;
-        double Yoffset;
+        public static final double Xoffset=0;
+        public static final double Yoffset=0;
+        public static final double driveID=0;
+        public static final double turnID=0;
+        public static final boolean driveInverted=false;
+        public static final boolean steerInverted=false;
     }
     public static class backRight{
-        double Xoffset;
-        double Yoffset;
+        public static final double Xoffset=0;
+        public static final double Yoffset=0;
+        public static final double driveID=0;
+        public static final double turnID=0;
+        public static final boolean driveInverted=false;
+        public static final boolean steerInverted=false;
     }
     public static class backLeft{
-        double Xoffset;
-        double Yoffset;
+        public static final double Xoffset=0;
+        public static final double Yoffset=0;
+        public static final double driveID=0;
+        public static final double turnID=0;
+        public static final boolean driveInverted=false;
+        public static final boolean steerInverted=false;
     }
     public static class shared{
-        double wheelRadius;
-        double driveGearRatio;
-        double turnGearRatio;
+        public static final double wheelRadius=0;
+        public static final double driveGearRatio=0;
+        public static final double turnGearRatio=0;
+        public static final double maxSpeed=0;
+    }
+
+    public static class driveTrainStuff{
+        public static final int pidgonID=0;
+        public static final SwerveDrivetrainConstants swerveDriveTrainConstants= new  SwerveDrivetrainConstants().withPigeon2Id(pidgonID);
     }
 }
