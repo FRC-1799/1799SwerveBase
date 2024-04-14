0fc73818f8383de096292638f61fdcefece335c2
twisterjafla <122408604+twisterjafla@users.noreply.github.com>
2024-04-13 18:12:29 -0600

    uh i forgot to commit stuff

---
+++ src/main/java/frc/robot/SwerveConstants.java
@@ -0,0 +1,28 @@
+package frc.robot;
+
+public class SwerveConstants {
+    public static class frontRight{
+        double Xoffset;
+        double Yoffset;
+        double driveID;
+        double turnID;
+
+    }
+    public static class frontLeft{
+        double Xoffset;
+        double Yoffset;
+    }
+    public static class backRight{
+        double Xoffset;
+        double Yoffset;
+    }
+    public static class backLeft{
+        double Xoffset;
+        double Yoffset;
+    }
+    public static class shared{
+        double wheelRadius;
+        double driveGearRatio;
+        double turnGearRatio;
+    }
+}
