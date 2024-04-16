fbc8f9f90179227f154fa4c5a471a183f5f1c373
twisterjafla <122408604+twisterjafla@users.noreply.github.com>
2024-04-16 17:02:52 -0600

    fuck swerve constants

--- src/main/java/frc/robot/SwerveConstants.java
+++ src/main/java/frc/robot/SwerveConstants.java
@@ -1,7 +1,13 @@
 package frc.robot;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstantsFactory;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.ClosedLoopOutputType;
+
+import frc.robot.CTRSwerve.SwerveDriveConstantsCreator;
+
 
 public class SwerveConstants {
     public static class frontRight{
@@ -11,8 +17,7 @@ public class SwerveConstants {
         public static final int turnID=0;
         public static final boolean driveInverted=false;
         public static final boolean steerInverted=false;
-        public static final SwerveModuleConstants frontRightConstants = new SwerveModuleConstants().withLocationX(Xoffset).withLocationY(Yoffset)
-        .withDriveMotorId(driveID).withSteerMotorId(turnID).withDriveMotorInverted(driveInverted).withSteerMotorInverted(steerInverted);
+
 
     }
     public static class frontLeft{
@@ -42,8 +47,24 @@ public class SwerveConstants {
     public static class shared{
         public static final double wheelRadius=0;
         public static final double driveGearRatio=0;
-        public static final double turnGearRatio=0;
+        public static final double steerGearRatio=0;
         public static final double maxSpeed=0;
+        public static final double driveVoltage=0;
+        public static final double driveInerta=0;
+        public static final double slipCurrent=0;
+        public static final Slot0Configs steerGains= new Slot0Configs();
+        public static final Slot0Configs driveGains = new Slot0Configs();
+        public static final boolean steerInverted=true;
+        public static final ClosedLoopOutputType driveMotorClosedLoop = null;
+        public static final ClosedLoopOutputType steerMotorClosedLoop = null;
+
+        
+
+
+        SwerveModuleConstantsFactory m_constantsCreator =
+        new SwerveModuleConstantsFactory().withCouplingGearRatio(driveGearRatio).withDriveFrictionVoltage(driveVoltage).withDriveInertia(driveInerta)
+        .withDriveMotorClosedLoopOutput(driveMotorClosedLoop).withDriveMotorGains(driveGains).withDriveMotorGearRatio(driveGearRatio).withFeedbackSource(null).withSlipCurrent(slipCurrent)
+        .withSpeedAt12VoltsMps(maxSpeed).withSteerMotorClosedLoopOutput(steerMotorClosedLoop).withSteerMotorGearRatio(steerGearRatio).withSteerMotorInverted(steerInverted).withWheelRadius(wheelRadius)
     }
 
     public static class driveTrainStuff{
